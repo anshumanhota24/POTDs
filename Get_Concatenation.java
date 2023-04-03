18bf663eb7931992d711ff9d13e4cf481d5b0cd9
anshumanhota24 <114644525+anshumanhota24@users.noreply.github.com>
2023-04-03 14:18:28 +0530

    Create Get_Concatenation.java

---
+++ Get_Concatenation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[nums.length*2];
+
+        for(int i=0;i<nums.length*2;i++){
+            if(i<nums.length){
+                ans[i]=nums[i];
+            }else
+            {
+                ans[i]=nums[i-nums.length];
+            }
+        }
+    return ans;
+    }
+}
+// https://leetcode.com/problems/concatenation-of-array
